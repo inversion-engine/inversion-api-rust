f53a2cfea0293df805cf034d4943286f8d9cd1b7
neonphog <neonphog@gmail.com>
2020-10-22 13:35:02 -0600

    Transfer Type

---
+++ crates/inversion_api_broker/src/lib.rs
@@ -0,0 +1,6 @@
+#![forbid(unsafe_code)]
+#![forbid(warnings)]
+#![forbid(missing_docs)]
+//! Core crates for loading and communicating with inversion api implementations in the Rust programming language.
+
+pub mod transfer;
